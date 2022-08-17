c3414bfc589f1cb013ef490f236255823e325acf
Tate Larkin <tatelarkin@Tates-MacBook-Pro.local>
2022-08-17 17:10:30 -0600

    Major UI Change

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+$(function () {
+  $(".preload").fadeOut(1000, function() {
+    $(".site-container").fadeIn(1000);        
+});
+})
+
 $(window).scroll(function () {
   let scroll = $(window).scrollTop();
   if (scroll > 0) {
