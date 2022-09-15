05e55c095c1e8f5fbe0d84bafc20cee49181985c
Tate Larkin <tatelarkin14@gmail.com>
2022-09-15 12:30:15 -0600

    change scroll animation code to intersection observer

--- index.js
+++ index.js
@@ -1,39 +1,21 @@
-const $animation_elements = $(".animation-element");
-const $window = $(window);
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('in-view')
+    } else {
+      entry.target.classList.remove('in-view')
+    }
+  })
+})
 
-$(function () {
-  $window.on("scroll resize", check_if_in_view);
-  $window.trigger("scroll", check_if_in_view);
-});
+const hiddenElements = document.querySelectorAll('.animation-element');
+hiddenElements.forEach((el) => observer.observe(el))
 
 $(window).scroll(function () {
   let scroll = $(window).scrollTop();
   if (scroll > 0) {
-    $("#navbar").addClass("navbar-scrolled");
+    document.querySelector('#navbar').classList.add('navbar-scrolled')
   } else {
-    $("#navbar").removeClass("navbar-scrolled");
+    document.querySelector('#navbar').classList.remove('navbar-scrolled')
   }
 });
-
-function check_if_in_view() {
-  let window_height = $window.height();
-  let window_top_position = $window.scrollTop();
-  let window_bottom_position = window_top_position + window_height;
-
-  $.each($animation_elements, function () {
-    let $element = $(this);
-    let element_height = $element.outerHeight();
-    let element_top_position = $element.offset().top;
-    let element_bottom_position = element_top_position + element_height;
-
-    //check to see if this current container is within viewport
-    if (
-      element_bottom_position >= window_top_position &&
-      element_top_position <= window_bottom_position
-    ) {
-      $element.addClass("in-view");
-    } else {
-      $element.removeClass("in-view");
-    }
-  });
-}
