9d9a89b7dcb5b8a9fa8cd301a3a6bbfe4e902888
Tate Larkin <tatelarkin14@gmail.com>
2024-05-24 19:02:21 -0600

    Circuit Craft listing

--- index.js
+++ index.js
@@ -35,70 +35,64 @@ class CircularArray {
   }
 }
 
-const carouselImageSources = ['./posters/siteReport.png', './posters/settings.png', './posters/extensionPopup.png']
-const circularArray = new CircularArray(carouselImageSources);
-const carouselNextButton = document.querySelector('.carousel-next')
-const carouselImage = document.querySelector('.carousel-image')
-const loader = document.querySelector('.loader')
-changeImgSource()
+function setupCarousel(carouselClassName, imageSources) {
+  const carouselContainers = document.querySelectorAll('.' + carouselClassName);
+  carouselContainers.forEach(carouselContainer => {
+    const circularArray = new CircularArray(imageSources);
+    const carouselNextButton = carouselContainer.querySelector('.carousel-next');
+    const carouselImage = carouselContainer.querySelector('.carousel-image');
+    const loader = carouselContainer.querySelector('.loader');
 
-carouselNextButton.addEventListener('click', () => {
-  updateCarouselDots()
-  changeImgSource()
-})
+    carouselNextButton.addEventListener('click', () => {
+      updateCarouselDots();
+      changeImgSource();
+    });
 
-function updateCarouselDots() {
-  const carouselDots = document.querySelector('.carousel-dots').children
-  for (let dot of carouselDots) {
-    dot.classList.remove('active')
-  }
-  let i = circularArray.currentIndex
-  carouselDots[i].classList.add('active')
-}
-
-function changeImgSource() {
-  carouselImage.classList.add('loading')
-  const image = new Image()
-  image.src = circularArray.next()
-
-  setTimeout(() => {
-    if (image.complete) {
-      if (!loader.classList.contains('hidden')) {
-        loader.classList.add('hidden');
+    function updateCarouselDots() {
+      const carouselDots = carouselContainer.querySelector('.carousel-dots').children;
+      for (let dot of carouselDots) {
+        dot.classList.remove('active');
       }
-      carouselImage.style.backgroundImage = `url('${image.src}')`;
-      carouselImage.classList.remove('loading');
-    } else {
-      if (loader.classList.contains('hidden')) {
-        loader.classList.remove('hidden')
-      }
-      image.addEventListener('load', () => {
-        if (!loader.classList.contains('hidden')) {
-          loader.classList.add('hidden');
-        }
-        carouselImage.style.backgroundImage = `url('${image.src}')`;
-        carouselImage.classList.remove('loading');
-      });
+      let i = circularArray.currentIndex;
+      carouselDots[i].classList.add('active');
     }
-  }, 150)
+
+    function changeImgSource() {
+      carouselImage.classList.add('loading');
+      const image = new Image();
+      image.src = circularArray.next();
+
+      setTimeout(() => {
+        if (image.complete) {
+          if (!loader.classList.contains('hidden')) {
+            loader.classList.add('hidden');
+          }
+          carouselImage.setAttribute('src', image.src)
+          carouselImage.classList.remove('loading');
+        } else {
+          if (loader.classList.contains('hidden')) {
+            loader.classList.remove('hidden');
+          }
+          image.addEventListener('load', () => {
+            if (!loader.classList.contains('hidden')) {
+              loader.classList.add('hidden');
+            }
+            carouselImage.style.backgroundImage = `url('${image.src}')`;
+            carouselImage.classList.remove('loading');
+          });
+        }
+      }, 150);
+    }
+
+    // Trigger loading of first source
+    changeImgSource();
+  });
 }
 
-// Image enlarging
-const enlargableImage = document.querySelector('.enlargable-image')
-enlargableImage.addEventListener('click', () => {
-  let src = enlargableImage.style.backgroundImage
-  const fullScreenImage = document.createElement('div')
-  fullScreenImage.style.background = 'rgba(0,0,0,.5) '+src+' no-repeat center'
-  fullScreenImage.style.backgroundSize = 'contain'
-  fullScreenImage.style.width = '100%'
-  fullScreenImage.style.height = '100%'
-  fullScreenImage.style.position = 'fixed'
-  fullScreenImage.style.zIndex = '9'
-  fullScreenImage.style.top = '0'
-  fullScreenImage.style.left = '0'
-  fullScreenImage.style.cursor = 'zoom-out'
-  fullScreenImage.addEventListener('click', (e) => {
-    e.target.remove()
-  })
-  document.body.appendChild(fullScreenImage)
-})
+const freshCarouselImageSources = ['./posters/siteReport.png', './posters/settings.png', './posters/extensionPopup.png']
+const freshCarouselClassname = 'fresh-carousel'
+setupCarousel(freshCarouselClassname, freshCarouselImageSources)
+
+const circuitCarouselImageSources = ['./posters/circuitLevel5.png', './posters/circuitHomeScreen.png', './posters/circuitSandbox.png']
+const circuitCarouselClassname = 'circuit-carousel'
+setupCarousel(circuitCarouselClassname, circuitCarouselImageSources)
